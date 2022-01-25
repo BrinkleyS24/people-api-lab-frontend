dd14db8386e2bd42b37de421ed005065913bee66
Stacey Brinkley <brinkleystacey12@gmail.com>
2022-01-25 15:34:57 -0500

    added form to create people

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,30 @@
 import { Link } from "react-router-dom"
+import { useState } from "react/cjs/react.development"
 
 function Index(props) {
+    // state to hold formData
+    const [newForm, setNewForm] = useState({
+        name: "",
+        image: "",
+        title: ""
+    });
+
+    // handleChange function for form
+    const handleChange = (event) => {
+        setNewForm({ ...newForm, [event.target.name]: event.target.value })
+    }
+
+    // handle submit function for form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.createPeople(newForm);
+        setNewForm({
+            name: "",
+            image: "",
+            title: "",
+        })
+    };
+
     const loaded = () => {
         return props.people.map((person) => (
             <div key={person._id} className="perosn">
@@ -12,8 +36,36 @@ function Index(props) {
     }
     const loading = () => {
         return <h1>Loading...</h1>;
-    }
-    return props.people ? loaded() : loading();
+    };
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={newForm.name}
+                    name="name"
+                    placeholder="name"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={newForm.image}
+                    name="image"
+                    placeholder="image URL"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={newForm.title}
+                    name="title"
+                    placeholder="title"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Create Person" />
+            </form>
+            {props.people ? loaded() : loading()}
+        </section>
+    );
 }
 
 export default Index;
